63a9c305410592dd70005b16c4c445ee1b7cc3ce
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2024-09-23 17:05:44 +0200

    Add trailing whitespace to test github action

--- src/OBMD/fix_obmd.cpp
+++ src/OBMD/fix_obmd.cpp
@@ -11,7 +11,7 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-#include "fix_obmd.h"
+#include "fix_obmd.h"    
 
 #include "atom.h"
 #include "atom_vec.h"
